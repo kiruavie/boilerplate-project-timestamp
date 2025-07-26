9074d16c4b23c6f77632f6c0c09400ade4615e5e
Harlem Kirua <debabel1hackamani@gmail.com>
2025-07-26 21:49:34 +0000

    fiexd bug

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ app.get("/", function (req, res) {
 
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api", function (req, res) {
   const date = new Date();
-  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
 });
 
 app.get("/api/:date", function (req, res) {
@@ -43,8 +50,6 @@ app.get("/api/:date", function (req, res) {
     utc: date.toUTCString(),
   });
 });
-
-app.get("/api/:date", function (req, res) {});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
